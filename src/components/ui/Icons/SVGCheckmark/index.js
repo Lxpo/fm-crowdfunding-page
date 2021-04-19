3131cd1c02530cd9e7f3721cbbab1fa7e802c1bb
LXPO\Lorenzo <enzoxo.ocampo7@yahoo.com>
2021-04-19 11:02:22 +0800

    Adusted svg icon size

--- src/components/ui/Icons/SVGCheckmark/index.js
+++ src/components/ui/Icons/SVGCheckmark/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import * as styled from './style'
+
+import Icon from '../../../../assets/icons/icon-check.svg'
 
 const SVGCheckIcon = () => {
     return (
-        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fillRule="evenodd">
-                <circle fill="#3CB3AB" cx="32" cy="32" r="32"/><path stroke="#FFF" strokeWidth="5" d="M20 31.86L28.093 40 44 24"/>
+        /* 
+            Resizing SVG based on IMG tag, for simple resizing
+            <styled.Image src={Icon}/>
+        */
+    
+        //Resizing image based on SVG tag, allows you to customize fill color etc.
+        <svg viewBox="-4 -3 70 70" width="90" height="80" preserveAspectRatio="xMidYMid" xmlns="http://www.w3.org/2000/svg" >
+            <g fill="none" fill-rule="evenodd">
+                <circle fill="#3CB3AB" cx="32" cy="32" r="32"/>
+                <path stroke="#FFF" stroke-width="5" d="M20 31.86L28.093 40 44 24"/>
             </g>
+            {/*Curly brace comments inside an HTML element.*/}
         </svg>
+        
     )
 }
+
+//you were gonna put the svg check icon into the icons folder
 
 export default SVGCheckIcon
--- src/components/ui/Icons/SVGCheckmark/style.js
+++ src/components/ui/Icons/SVGCheckmark/style.js
@@ -1,2 +1,9 @@
 import styled from 'styled-components'
-import * as sg from '../../util/styleGuide'
+import * as sg from '../../../util/styleGuide'
+
+export const Image = styled.img`
+    max-width:70px;
+    width:100%;
+    max-height:70px;
+    height:100%;
+`
